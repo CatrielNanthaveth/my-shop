03546aa0b7eb17eb1dacc49f3730dec16c3dafa5
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2023-04-12 20:38:03 -0300

    header terminado
    se creó el header, se agregó a la ruta y se hizo el css del mismo.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import { useState } from 'react'
 import './header.css';
+import { Link } from 'react-router-dom';
+import logo from "../../resources/logo.png"
 
 export const Header = () => {
+    const [search, setSearch] = useState("");
 
-    let headerList = ["Inicio", "Mis compras", "Favoritos", "Mi carro"];
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    let headerList = [
+        {
+            name: "Inicio",
+            link: "/home"
+        }, {
+            name: "Mis compras",
+            link: "/my_purcharses"
+        }, {
+            name: "Favoritos",
+            link: "/favorites"
+        }, {
+            name: "Mi carro",
+            link: "/cart"
+        }];
 
     return (
-        <header>
+        <header id='header'>
+            <img src={logo} id='logo-navbar' alt='logo' />
+
+            <div id='search'>
+                <input
+                    value={search}
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="Buscar"
+                    id="search-bar"
+                />
+            </div>
+
             <ul id='headerList'>
                 {headerList.map((item) => {
                     return (
-                        <li>{item}</li>
+                        <li><Link to={item.link}>{item.name}</Link></li>
                     )
                 })}
             </ul>
