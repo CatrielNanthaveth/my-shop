118bdb268a5134d8663271f2080ca9536759dbf3
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2023-05-02 16:41:22 -0300

    creación de productos

---
+++ src/resources/productos.js
@@ -0,0 +1,65 @@
+import { img1 } from "./productos_img/jogging_suelto.png";
+import { img2 } from "./productos_img/cargo.png";
+import { img3 } from "./productos_img/jean.png";
+import { img4 } from "./productos_img/buzo_trasher.png";
+import { img5 } from "./productos_img/buzo_groovy.png";
+import { img6 } from "./productos_img/buzo_design_art.png";
+import { img7 } from "./productos_img/remera_smile.png";
+import { img8 } from "./productos_img/remera_beige.png";
+import { img9 } from "./productos_img/remera_tiger.png";
+
+const productos = [
+    {
+        pantalones: [
+            {
+                name: "Jogging",
+                info: "Jogging suelto a la moda",
+                img: {img1}
+            },
+            {
+                name: "Cargo",
+                info: "Cargo blanco",
+                img: {img2}
+            },
+            {
+                name: "Jean",
+                info: "Jean negro clásico",
+                img: {img3}
+            }
+        ],
+        buzos: [
+            {
+                name: "Buzo Trasher",
+                info: "Buzo con logo Trasher",
+                img: {img4}
+            },
+            {
+                name: "Buzo Groovy",
+                info: "Buzo con logo Groovy",
+                img: {img5}
+            },
+            {
+                name: "Buzo DesignArt",
+                info: "Buzo con logo DesignArt",
+                img: {img6}
+            }
+        ],
+        remeras: [
+            {
+                name: "Remera Smile",
+                info: "Remera con logo de Smile",
+                img: {img7}
+            },
+            {
+                name: "Remera lisa",
+                info: "Remera lisa color beige",
+                img: {img8}
+            },
+            {
+                name: "Remera Tiger",
+                info: "Remera con logo de tigre",
+                img: {img9}
+            }
+        ]
+    }
+]
